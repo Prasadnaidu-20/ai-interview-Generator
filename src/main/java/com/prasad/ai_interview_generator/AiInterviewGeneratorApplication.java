04c45fd1279fe1c81f9ae608d89a513d1168a0a1
Prasadnaidu-20 <bobbynaidu2005@gmail.com>
2026-03-08 08:05:37 +0530

    finished setting up spring application

---
+++ src/main/java/com/prasad/ai_interview_generator/AiInterviewGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.prasad.ai_interview_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiInterviewGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiInterviewGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prasad/ai_interview_generator/AiInterviewGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prasad.ai_interview_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiInterviewGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
